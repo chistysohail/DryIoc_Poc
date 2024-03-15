8de0daa19f1fd5d536b0175a3412ef72ebaeea87
chisty sohel <chistysohail@gmail.com>
2024-03-15 14:24:49 -0400

    Add project files.

---
+++ DryIoc_Poc/Program.cs
@@ -0,0 +1,34 @@
+﻿using DryIoc;
+using System;
+using System.ComponentModel;
+
+namespace DryIocExample
+{
+    public interface IService
+    {
+        void Serve();
+    }
+
+    public class Service : IService
+    {
+        public void Serve()
+        {
+            Console.WriteLine("Service Called");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = new DryIoc.Container();
+            container.Register<IService, Service>();
+
+            var service = container.Resolve<IService>();
+            service.Serve();
+        }
+    }
+}
+
+
+//dotnet add package DryIoc.dll
